2b112b65e2a7d1a314ce420fdf4bd26209bfe833
liammather97 <liammather97@gmail.com>
2020-01-09 18:06:15 +0000

    fixed bugs

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -7,10 +7,10 @@ def home():
     return render_template('home.html')
 
 @app.route('/generator', methods=['GET', 'POST'])
-def gen():
+def generator():
     form = NameForm()
     if request.method == 'POST':
         return form.name.data
 
-    return render_template('generator.html', title='Enter your name: ', form=form)
+    return render_template('generator.html', title='Generator', form=form)
 
