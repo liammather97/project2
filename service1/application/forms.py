7cb3a74d6b12c0640c1070c80d800a64d46df807
liammather97 <liammather97@gmail.com>
2020-01-09 17:17:01 +0000

    debugging

--- service1/application/forms.py
+++ service1/application/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,16 +1,16 @@
-from app import application
-from flask import render_template
+from application import app
+from flask import render_template, request
 from application.forms import NameForm
 
-@app.route('/', '/home')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-
+    return render_template('home.html')
 
 @app.route('/generator', methods=['GET', 'POST'])
 def gen():
     form = NameForm()
     if request.method == 'POST':
-        return form.name
+        return form.name.data
 
     return render_template('generator.html', title='Enter your name: ', form=form)
 
