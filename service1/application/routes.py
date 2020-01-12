a649a2537f3c709230975b980e363c7b82516100
liammather97 <liammather97@gmail.com>
2020-01-12 22:46:20 +0000

    error

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -11,10 +11,10 @@ def home():
 def generator():
     form = NameForm()
     if form.validate_on_submit:
-        r2 = requests.get("http://service2:5002/")
-        r3 = requests.get("http://service3:5003/")
+        r2 = requests.get("http://service2:5002")
+        r3 = requests.get("http://service3:5003")
         data = {"prefix":r2["choice"],"suffix":r3["choice"],"user_input":form.name.data}
-        r4 = requests.post("http://service4:5004/", json=data)
+        r4 = requests.post("http://service4:5004", json=data)
         return r4["Result"]
     return render_template('generator.html', title='Generator', form=form)
 
