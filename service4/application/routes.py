9140bd5d2c6eb69fae938d6ea1fd8c955bd736fa
liammather97 <liammather97@gmail.com>
2020-01-13 14:06:33 +0000

    pls

--- service4/application/routes.py
+++ service4/application/routes.py
@@ -5,8 +5,8 @@ import json
 @app.route('/')
 @app.route('/home',methods=('GET','POST'))
 def home(json):
-    request.get(service2).json()
-    request.get(service3).json()
+    prefix1 = request.get(service2).json()
+    suffix1 = request.get(service3).json()
     user_input=request.get_json('data')
     combined = prefix1 + user_input + suffix1
     result = jsonify({"Result":combined})
